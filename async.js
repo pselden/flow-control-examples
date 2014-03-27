ff21e878d61880bc4ad863524247309a90625ff1
Paul Selden <pselden@users.noreply.github.com>
2014-03-27 12:17:46 -0400

    Cleanup

--- async.js
+++ async.js
@@ -1,31 +1,31 @@
 var async = require('async');
 
 function getValue(id, callback){
-  function getFromCache(callback){
-    cache.get(id, callback);
-  }
+    function getFromCache(callback){
+        cache.get(id, callback);
+    }
 
-  function returnOrFetch(cachedValue, callback){
-    if(cachedValue){ return callback(null, cachedValue); }
+    function returnOrFetch(cachedValue, callback){
+        if(cachedValue){ return callback(null, cachedValue); }
 
-    fetchAndCache(id, callback);
-  }
+        fetchAndCache(id, callback);
+    }
 
-  function fetchAndCache(callback){
+    function fetchAndCache(callback){
+        async.waterfall([
+            function (callback){
+                db.query('SELECT * FROM values WHERE id = $1', [id], callback);
+            },
+            function (dbValue, callback){
+                cache.put(id, dbValue, function(){
+                    callback(null, dbValue);
+                });
+            }
+        ], callback);
+    }
+
     async.waterfall([
-      function (callback){
-        db.query('SELECT * FROM values WHERE id = $1', [id], callback);
-      },
-      function (dbValue, callback){
-        cache.put(id, dbValue, function(){
-          callback(null, dbValue);
-        });
-      }
+        getFromCache,
+        returnOrFetch,
     ], callback);
-  }
-    
-  async.waterfall([
-    getFromCache,
-    returnOrFetch,
-  ], callback);
 }
