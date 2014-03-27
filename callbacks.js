c50074b63c73270d3b54cd9dac32bdb1457c6b7f
Paul Selden <pselden@users.noreply.github.com>
2014-03-27 12:18:02 -0400

    Cleanup

--- callbacks.js
+++ callbacks.js
@@ -1,11 +1,11 @@
 function getValue(id, callback) {
-  cache.get(id, function (err, cachedValue) {
-    if (cachedValue !== null) { return callback(null, cachedValue); }
-    
-    db.get('SELECT * FROM values WHERE id = $1', [id], function (err, dbValue) {
-      cache.put(id, dbValue, function () {
-        callback(null, dbValue);
-      });
+    cache.get(id, function (err, cachedValue) {
+        if (cachedValue !== null) { return callback(null, cachedValue); }
+
+        db.get('SELECT * FROM values WHERE id = $1', [id], function (err, dbValue) {
+            cache.put(id, dbValue, function () {
+                callback(null, dbValue);
+            });
+        });
     });
-  });
 }
