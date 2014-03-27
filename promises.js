176ba71828d896d423ff32a82eea0ecf89b36d03
Paul Selden <pselden@users.noreply.github.com>
2014-03-27 12:17:23 -0400

    Cleanup

--- promises.js
+++ promises.js
@@ -1,24 +1,25 @@
 function getValue(id, callback){
-  getFromCache(id)
-  .then(function(cachedValue){
-  if(cachedValue){ return cachedValue; }
-    getFromDb(id)
-    .then(function(dbValue){
-      saveToCache(id, dbValue);
-      return dbValue;
-    })
-  })
-  .done(callback);
+    getFromCache(id)
+        .then(function(cachedValue){
+           if(cachedValue){ return cachedValue; }
+        
+           getFromDb(id)
+               .then(function(dbValue){
+                   saveToCache(id, dbValue);
+                   return dbValue;
+               })
+        })
+        .done(callback);
 }
 
 function getFromCache(id){
-  return cache.get(id);
+    return cache.get(id);
 }
 
 function getFromDb(id){
-  return db.query("SELECT * FROM values WHERE id = $1", [id]);
+    return db.query("SELECT * FROM values WHERE id = $1", [id]);
 }
 
 function saveToCache(id, value){
-  return cache.put(id, value);
+    return cache.put(id, value);
 }
