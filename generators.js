7c8c46170a3f1227c8f55035531072d7499968d9
Paul Selden <pselden@users.noreply.github.com>
2014-03-28 10:28:50 -0400

    Update generators.js

--- generators.js
+++ generators.js
@@ -1,7 +1,7 @@
 function * getValue(id){
     var cachedValue = yield cache.get(id);
     if(cachedValue !== null) { return cachedValue; }
-    var dbValue = yield db.query('SELECT * FROM values WHERE id == $1', [id]);
+    var dbValue = yield db.query('SELECT * FROM values WHERE id = $1', [id]);
     yield cache.put(id, dbValue);
     return dbValue;
 }
